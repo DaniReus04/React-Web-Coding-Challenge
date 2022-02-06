121e51955395c115027a1ee9be3696f9ff71c2e5
DaniReus04 <daniel_carvalho_amaroo@outlook.com>
2022-02-06 20:54:42 -0300

    link back to home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Logo from "../../Assets/Images/berlin-police-logo.png";
+import { Link } from "react-router-dom";
 
 function header() {
   return (
     <div className="flex w-full">
-      <img src={Logo} alt="police-logo" className="w-32 py-6" />
+      <Link to="/">
+        <img src={Logo} alt="police-logo" className="w-32 py-6" />
+      </Link>
       <div className="flex flex-col justify-center px-6 w-full">
         <p className="text-5xl font-semibold">Police Departament of Berlin</p>
         <p className="text-2xl font-semibold">Stolen bykes</p>
